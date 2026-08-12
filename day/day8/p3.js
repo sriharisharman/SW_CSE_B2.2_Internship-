27924cee22f3b904c59d7c791a29607bd8f56584
sriharisharman <sriharisharma18@gmail.com>
2026-08-12 12:26:35 +0530

    JS Datatypes

---
+++ day/day8/p3.js
@@ -0,0 +1,51 @@
+// js Datatypes
+// Primitive Data Type
+    // 1. string
+    // 2. Number
+    // 3. Bigint
+    // 4. Boolean
+    // 5. Undefined
+    // 6. Null
+    // 7. Symbol
+// non primitive data types 
+    // 1. object
+    // 2. array
+    // 3. function
+    
+
+    // 1. string
+    let name = "Sri Hari Sharma"; 
+    // 2. Number
+    let age = 20;
+    // 3. Bigint
+    let bignumber = 125478963;
+    // 4. Boolean
+    let isnumber = true;
+    // 5. Undefined
+    let value;
+    // 6. Null
+    let attendance = null;
+    // 7. Symbol
+    let id = Symbol("id");
+
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof BigInt);
+    console.log(typeof isnumber);
+    console.log(typeof value);
+    console.log(typeof attendance);
+    console.log(typeof id);
+  
+// non primitive data types 
+    // 1. object
+    let person = {
+        name = "Sri Hari Sharma",
+        age= 36
+    };
+    // 2. array
+    let fruits = ["apple", "mango", "banana"]
+    // 3. function
+    function greet(){
+        return "Hello";
+    }
+    
