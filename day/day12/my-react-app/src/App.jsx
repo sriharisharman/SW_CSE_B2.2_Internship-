378809c88f5348aa108fed4074c6bb00e07bec53
sriharisharman <sriharisharma18@gmail.com>
2026-08-18 13:00:17 +0530

    Function function | export default

--- day/day12/my-react-app/src/App.jsx
+++ day/day12/my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import { Welcome } from "./components/p1";
-
+import Greet from "./components/p2";
 function App() {
 
   return (
     <div>
     <h1>Hello My First React App</h1>
     <Welcome/>
+    <Greet />
     <p>Return should only return one parent tag. Within a single parent atg there can be n number of tags.</p>
     </div>
   )
---
+++ day/day12/my-react-app/src/components/p2.jsx
@@ -0,0 +1,8 @@
+export default function Greet(){
+    return(
+        <>
+            <h6>Hello My name is sri hari sharama.N</h6>
+            <p> I am learning React JS now.</p>
+        </>
+    )
+}
