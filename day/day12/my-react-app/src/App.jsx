9b4331f6ff5ee34b289f3fd2cd4e931e667730e7
sriharisharman <sriharisharma18@gmail.com>
2026-08-18 12:46:40 +0530

    Functional Component(named export)

--- day/day12/my-react-app/src/App.jsx
+++ day/day12/my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-
+import { Welcome } from "./components/p1";
 
 function App() {
 
   return (
+    <div>
     <h1>Hello My First React App</h1>
+    <Welcome/>
+    <p>Return should only return one parent tag. Within a single parent atg there can be n number of tags.</p>
+    </div>
   )
 }
 
