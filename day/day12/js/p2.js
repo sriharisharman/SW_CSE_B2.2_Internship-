40823316d4ca5827edc17ec9130b85c71947b29c
sriharisharman <sriharisharma18@gmail.com>
2026-08-18 12:20:21 +0530

    named export & default export

---
+++ day/day12/js/p2.js
@@ -0,0 +1,7 @@
+import { add,subtract } from "./P1.js"; //Named export
+import multiply from "./P3.js"; //default export
+
+console.log(add(20,20));
+console.log(subtract(100,65));
+
+console.log(multiply(20,10));
