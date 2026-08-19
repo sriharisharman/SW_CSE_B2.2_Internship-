3cb1a27cb24765690d02b40f49496672f8c9551d
sriharisharman <sriharisharma18@gmail.com>
2026-08-19 10:57:04 +0530

    Passing multiple props

--- day/day13/day-13-react-app/src/App.jsx
+++ day/day13/day-13-react-app/src/App.jsx
@@ -1,10 +1,17 @@
 import { Welcome } from "./components/p1";
-
+import { ProductCard } from "./components/p2";
+import './App.css'
+import lenovo from "./assets/lenovo_loq_rtx_4050.jpg"
 function App() {
   
   return (
     <>
     <Welcome name ="SriHari"/>
+    <ProductCard
+    name="Lenovo LOQ 2024, Intel Core i5-13450HX, 13th Gen"
+    price={125000}
+    image={lenovo}
+    />
 
     </>
   )
